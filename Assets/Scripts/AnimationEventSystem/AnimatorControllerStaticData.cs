a43bf3b9a57efddab290a7ab110a2e812c4c46d1
Choccy <yarlouz10@gmail.com>
2023-02-16 13:28:06 +0700

    Weapon Container Script

--- Assets/Scripts/AnimationEventSystem/AnimatorControllerStaticData.cs
+++ Assets/Scripts/AnimationEventSystem/AnimatorControllerStaticData.cs
@@ -9,33 +9,6 @@ namespace AnimationEventSystem
 	[CreateAssetMenu]
 	public class AnimatorControllerStaticData : ScriptableObject
 	{
-		// Token: 0x0600575A RID: 22362 RVA: 0x001B1601 File Offset: 0x001AF801
-		[CanBeNull]
-		public List<AnimationEvent> GetEventsByIndex(int index)
-		{
-			if (this.IsValidListIndex(index))
-			{
-				return this._stateHashToEventsCollection[index].AnimationEvents;
-			}
-			Debug.LogErrorFormat("Invalid index for {0}", new object[]
-			{
-				index
-			});
-			return null;
-		}
-
-		// Token: 0x0600575B RID: 22363 RVA: 0x001B1638 File Offset: 0x001AF838
-		public LActionSetup GetLActionSetupById(int lActionSetupId)
-		{
-			return this._stateHashToLActionSetups[lActionSetupId];
-		}
-
-		// Token: 0x0600575C RID: 22364 RVA: 0x001B1646 File Offset: 0x001AF846
-		public bool IsValidListIndex(int index)
-		{
-			return index >= 0 && index < this._stateHashToEventsCollection.Count;
-		}
-
 		// Token: 0x04005844 RID: 22596
 		[SerializeField]
 		private List<EventsCollection> _stateHashToEventsCollection;
--- Assets/Scripts/AnimationEventSystem/EventsCollection.cs
+++ Assets/Scripts/AnimationEventSystem/EventsCollection.cs
@@ -8,38 +8,7 @@ namespace AnimationEventSystem
 	[Serializable]
 	public class EventsCollection
 	{
-		// Token: 0x06005754 RID: 22356 RVA: 0x001B153C File Offset: 0x001AF73C
-		public EventsCollection(IEnumerable<AnimationEvent> animationEvents)
-		{
-			this._animationEvents = new List<AnimationEvent>(animationEvents);
-		}
-
-		// Token: 0x17000B49 RID: 2889
-		// (get) Token: 0x06005755 RID: 22357 RVA: 0x001B1550 File Offset: 0x001AF750
-		public List<AnimationEvent> AnimationEvents
-		{
-			get
-			{
-				return this._animationEvents;
-			}
-		}
-
-		// Token: 0x06005756 RID: 22358 RVA: 0x001B1558 File Offset: 0x001AF758
-		private static void smethod_0(EventsCollection eventsCollection)
-		{
-			float max = 0.9899901f;
-			List<AnimationEvent> animationEvents = eventsCollection._animationEvents;
-			for (int i = 0; i < animationEvents.Count; i++)
-			{
-				AnimationEvent animationEvent = animationEvents[i];
-				float num = Mathf.Clamp(animationEvent.Time, 0f, max);
-				if (Math.Abs(num - animationEvent.Time) >= 1E-45f)
-				{
-					animationEvent.Time = num;
-				}
-			}
-		}
-
+		
 		// Token: 0x04005840 RID: 22592
 		[SerializeField]
 		private List<AnimationEvent> _animationEvents;
