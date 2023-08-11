37bacfbab8f59decb16ae280359103a032a9328a
Choccy <yarlouz10@gmail.com>
2023-08-11 13:46:42 +0700

    Update weap related scrpt + add belt script + misc

---
+++ Assets/Scripts/AnimationEventSystem/AnimationEventsSequenceData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnimationEventSystem
+{
+	// Token: 0x02000EFA RID: 3834
+	public class AnimationEventsSequenceData
+	{
+
+		// Token: 0x04005BE7 RID: 23527
+		private const int MAX_QUEUE_SIZE = 15;
+
+		// Token: 0x04005BE8 RID: 23528
+		public readonly Queue<AnimationEventsSequenceData.GStruct108> AnimationEventsDebugQueue = new Queue<AnimationEventsSequenceData.GStruct108>(15);
+
+		// Token: 0x02000EFB RID: 3835
+		public readonly struct GStruct108
+		{
+			// Token: 0x06005D94 RID: 23956 RVA: 0x001C7BF9 File Offset: 0x001C5DF9
+			public GStruct108(string eventName, int stateHash, bool condsPassed)
+			{
+				this.EventName = eventName;
+				this.StateNameShortHash = stateHash;
+				this.ConditionPassed = condsPassed;
+			}
+
+			// Token: 0x04005BE9 RID: 23529
+			public readonly string EventName;
+
+			// Token: 0x04005BEA RID: 23530
+			public readonly int StateNameShortHash;
+
+			// Token: 0x04005BEB RID: 23531
+			public readonly bool ConditionPassed;
+		}
+	}
+}
---
+++ Assets/Scripts/ModPlacer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+// Token: 0x020005EA RID: 1514
+public class ModPlacer : MonoBehaviour
+{
+	// Token: 0x04002226 RID: 8742
+	public Vector3 ModPosition = Vector3.zero;
+
+	// Token: 0x04002227 RID: 8743
+	public Vector3 ModRotation = Vector3.zero;
+
+	// Token: 0x04002228 RID: 8744
+	public Vector3 ModScale = Vector3.one;
+}
