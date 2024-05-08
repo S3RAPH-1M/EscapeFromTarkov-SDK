f827a7762955f46328ee410fba9c8969892798c4
GrooveypenguinX <jakechase2110@gmail.com>
2024-05-08 10:00:22 -0400

    voice scripts

---
+++ Assets/Scripts/SpreadGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommonAssets.Scripts.Audio;
+
+[Serializable]
+public class SpreadGroup
+{
+	public ELoudnessType LoudnessType = ELoudnessType.Normal;
+
+	public int Falloff = 50;
+
+	public float Volume = 1f;
+
+	public TaggedClip[] Clips = new TaggedClip[0];
+}
