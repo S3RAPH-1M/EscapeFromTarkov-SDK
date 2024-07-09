440de8a41ebad04dea61ddd87f0ebce4cfb53244
SSH_ <servph@servphcorpp.com>
2024-07-08 22:45:14 -0400

    Hooded Dress Implementation + Organization

---
+++ Assets/Scripts/EFT/Visual/HoodedDress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace EFT.Visual
+{
+	// Token: 0x02001AC3 RID: 6851
+	public class HoodedDress : MonoBehaviour
+	{
+		public bool IsHooded { get; set; }
+		[SerializeField]
+		private GameObject[] _hooded;
+
+		[SerializeField]
+		private GameObject[] _notHooded;
+
+		[CompilerGenerated]
+		private bool bool_0;
+	}
+}
